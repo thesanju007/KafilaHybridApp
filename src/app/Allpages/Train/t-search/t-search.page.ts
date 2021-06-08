4e5ce379448c60dff7bdb9b3ea10b9650f585488
thesanju007 <admin@kafilatravel.in>
2021-06-08 18:04:49 +0530

    ankit

--- src/app/Allpages/Train/t-search/t-search.page.ts
+++ src/app/Allpages/Train/t-search/t-search.page.ts
@@ -68,7 +68,81 @@ export class TSearchPage implements OnInit {
    
     console.log(this.flightData.value)
   }
+  isShown : Boolean = false;
+  showflight : Boolean = true;
+  showhotel : Boolean = false;
+  showtrain : Boolean = false;
+  showlogin : Boolean = false;
+  show : Boolean = true;
+  isSelected_F: any = true;
+  isSelected_T: any = false;
+  isSelected_H: any = false;
+  toggleStyle : boolean = false;
+  value:any;
+  
+ 
+
+  station_details= new FormGroup({
+ 
+    stationfrom:new FormControl('',[Validators.required]),
+    stationto:new FormControl('',[Validators.required]),
    
+   });
+
+
+   travel_date= new FormGroup({
+ 
+    deptdate:new FormControl('',[Validators.required]),
+    retdate:new FormControl('',[Validators.required]),
+   
+   });
+
+   no_of_passengers= new FormGroup({
+    adults:new FormControl('',[Validators.required]),
+    childs:new FormControl('',[Validators.required]),
+
+   });
+
+
+  toggleShow() {
+    this.isShown = true;
+  }
+
+  toggleShow1() {
+    this.isShown = false;
+  }
+
+  login() {
+    this.showlogin = !this.showlogin;
+    this.show = !this.show;
+  }
+
+  toggleflight() {
+    this.showflight= true;
+    this.showhotel = false;
+    this.showtrain = false;
+    this.isSelected_F = true;
+    this.isSelected_T = false;
+    this.isSelected_H = false;
+  }
+  
+  togglehotel() {
+    this.showflight = false;
+    this.showhotel = true;
+    this.showtrain = false;
+    this.isSelected_H = true;
+    this.isSelected_F = false;
+    this.isSelected_T = false;
+  }
+
+  toggletrain() {
+    this.showflight = false;
+    this.showhotel = false;
+    this.showtrain = true;
+    this.isSelected_F = false;
+    this.isSelected_T = true;
+    this.isSelected_H = false;
+  } 
          
  
 
