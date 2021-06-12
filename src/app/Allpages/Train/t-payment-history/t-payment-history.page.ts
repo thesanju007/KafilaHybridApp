ae46be5b41d3862334f0fa222b9364340ec1a10c
thesanju007 <admin@kafilatravel.in>
2021-06-12 11:24:53 +0530

    shivam commit

--- src/app/Allpages/Train/t-payment-history/t-payment-history.page.ts
+++ src/app/Allpages/Train/t-payment-history/t-payment-history.page.ts
@@ -6,18 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./t-payment-history.page.scss'],
 })
 export class TPaymentHistoryPage implements OnInit {
-  message={
-  AGENCY_NAME:"KAFILA",
-  AGENT_ID:"18785869",
-  P_TYPE:"AIR",
-  AMOUNT:0
+  message = {
+    AGENCY_NAME: "KAFILA",
+    AGENT_ID: "18785869",
+    P_TYPE: "AIR",
+    AMOUNT: 0
   };
   constructor() { }
 
   ngOnInit() {
   }
-onConfirm()
-{
-  
-}
+  onConfirm() {
+    let data=this.message
+  }
 }
